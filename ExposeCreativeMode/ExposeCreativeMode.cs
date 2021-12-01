aad0f07d676277459b019bd65ee80fbbe92827ce
Raptor <Raptor@RAPTORXPS>
2021-12-01 18:39:08 +0530

    Add project files.

---
+++ ExposeCreativeMode/ExposeCreativeMode.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace DysonSphereProgram.Modding.ExposeCreativeMode
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    [BepInProcess("DSPGAME.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string GUID = "dev.raptor.dsp.ExposeCreativeMode";
+        public const string NAME = "ExposeCreativeMode";
+        public const string VERSION = "0.0.2";
+
+        private Harmony _harmony;
+        internal static ManualLogSource Log;
+
+        private void Awake()
+        {
+            Plugin.Log = Logger;
+            _harmony = new Harmony(GUID);
+            _harmony.PatchAll(typeof(PlayerController__Init));
+            _harmony.PatchAll(typeof(PlayerAction_Test__Update));
+            Logger.LogInfo("ExposeCreativeMode Awake() called");
+        }
+
+        private void OnDestroy()
+        {
+            Logger.LogInfo("ExposeCreativeMode OnDestroy() called");
+            _harmony?.UnpatchSelf();
+            Plugin.Log = null;
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerController), nameof(PlayerController.Init))]
+    class PlayerController__Init
+    {
+        [HarmonyPostfix]
+        static void Postfix(PlayerController __instance)
+        {
+            // We do a bit of extra stuff because when using it with ScriptEngine from
+            // BepInEx.Debug, we might end up patching the method multiple times
+
+            // If the last entry in the actions array isn't an instance of creative mode's base class, allocate space for it
+            if (!(__instance.actions[__instance.actions.Length - 1] is PlayerAction_Test))
+            {
+                var newActions = new PlayerAction[__instance.actions.Length + 1];
+                __instance.actions.CopyTo(newActions, 0);
+                __instance.actions = newActions;
+            }
+
+            // Overwrite the last action with the latest creative mode code
+            var creativeMode = new PlayerAction_CreativeMode();
+            creativeMode.Init(__instance.player);
+            __instance.actions[__instance.actions.Length - 1] = creativeMode;
+
+            Debug.Log("Creative Mode Postfix patch applied");
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerAction_Test), nameof(PlayerAction_Test.Update))]
+    public class PlayerAction_Test__Update
+    {
+        static CodeMatch[] KeypadNum_PatchSite(KeyCode k) => new[]
+        {
+              new CodeMatch(OpCodes.Ldc_I4, (int)k)
+            , new CodeMatch(OpCodes.Call, AccessTools.Method(typeof(Input), nameof(Input.GetKeyDown), new [] { typeof(KeyCode) }))
+            , new CodeMatch(ci => ci.opcode == OpCodes.Brfalse)
+        };
+
+        static CodeMatcher PatchLeftControlSkipOverrideForKeypadNum(CodeMatcher matcher, KeyCode k)
+        {
+            matcher.MatchForward(true, KeypadNum_PatchSite(k));
+            var ifBlockExitLabel = matcher.Operand;
+
+            matcher
+                .Advance(1)
+                .Insert(
+                      new CodeInstruction(OpCodes.Ldc_I4, (int)KeyCode.LeftControl)
+                    , new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Input), nameof(Input.GetKey), new[] { typeof(KeyCode) }))
+                    , new CodeInstruction(OpCodes.Brtrue, ifBlockExitLabel)
+                );
+
+            return matcher;
+        }
+
+        static CodeMatcher PlayWithIL(List<CodeInstruction> il_copy, ILGenerator generator)
+        {
+            var matcher = new CodeMatcher(il_copy, generator);
+
+            PatchLeftControlSkipOverrideForKeypadNum(matcher, KeyCode.Keypad1);
+            PatchLeftControlSkipOverrideForKeypadNum(matcher, KeyCode.Keypad0);
+
+            return matcher;
+        }
+
+        const bool DryRun = false;
+
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            var il_copy = new List<CodeInstruction>(instructions);
+            try
+            {
+                PlayWithIL(il_copy, generator);
+            }
+            catch (Exception e)
+            {
+                Plugin.Log.LogError(e);
+            }
+
+            if (DryRun)
+            {
+                foreach (var ins in il_copy)
+                    yield return ins;
+            }
+            else
+            {
+                var modified_ins = PlayWithIL(il_copy, generator).InstructionEnumeration();
+                foreach (var ins in modified_ins)
+                    yield return ins;
+            }
+        }
+    }
+}
---
+++ ExposeCreativeMode/Thunderstore/README.md
@@ -0,0 +1,50 @@
+# ExposeCreativeMode Mod
+This mod merely exposes the "test mode" left in the game by the devs as "creative mode" and adds some text in the UI below the version and player info to indicate when creative mode is enabled.
+
+## How to use this mod
+* First install the mod, and reboot the game.
+* Press Shift+F4 to toggle creative mode
+* When in creative mode
+  * Ctrl + T : Unlocks all techs
+  * Ctrl + A : Resets all achievements
+  * Numpad 1 : Places one stack of most items in the game to the inventory.
+  * Numpad 2 : Increases Mecha mining, walking, replicating and reactor speed to a fixed high value. Increases research speed to a fixed high value
+  * Numpad 3 : Covers the current planet in foundation entirely
+  * Numpad 4 : Increases construction drone count by 1
+  * Numpad 5 : Sets drone speed to a fixed high value
+  * Numpad 6 : Immediately researches the current queued tech
+  * Numpad 7 : Unlocks sail mode
+  * Numpad 8 : Sets energy to 20 GJ. Also unlocks sail mode
+  * Numpad 9 : Unlocks warp
+  * Numpad 0 : Sets all station storage to 100000000
+  * Numpad + : Adds more of the currently selected building to the inventory
+
+  My additional functions
+  * Ctrl + Numpad 1 : Toggles infinite inventory mode  
+  (**WARNING**: Saving while in infinite inventory mode, will permanently change the player inventory for the particular save - meaning you won't be able to toggle back after relaunching DSP)
+  * Ctrl + Numpad 0 : Toggles infinite station mode  
+  (**WARNING**: This probably does not do what you think it does. Read the description carefully before trying it out)
+
+### Infinite Inventory mode
+
+* Makes the player inventory have all items present in the game
+* All items are reset to 9999, with the stack size being 30000 every frame
+* Because the stack is greater, items which go into the inventory are effectively "deleted"
+
+### Infinite Station mode
+
+* This mode is primarily for testing blueprints, by making stations infinitely output to belt and consume from belt without needing any drones/vessels
+* Makes all stations set to supply have items less than half of max
+* Makes all stations set to demand have items more than half of max
+* In the case of ILS, only remote supply/demand is checked. If remote is set to storage, then local is taken
+
+## Changelog
+
+### v0.0.2
+
+* Uploaded source code to GitHub  
+* Added a couple of functions for testing blueprints and mods
+
+### v0.0.1
+
+* Initial version that just exposes already existing mode
