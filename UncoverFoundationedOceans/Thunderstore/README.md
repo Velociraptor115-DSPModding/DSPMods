fb8bf41242442e73d0322f1585b916b5c36afa23
Velociraptor115 <Velociraptor115@users.noreply.github.com>
2022-01-21 18:57:54 +0530

    Add UncoverFoundationedOceans mod

---
+++ UncoverFoundationedOceans/Thunderstore/README.md
@@ -0,0 +1,16 @@
+# UncoverFoundationedOceans Mod
+This mod allows you to uncover oceans that you have paved over.  
+The way it works is to reset the terrain height to the original and reverse the amount of soil used in the process  
+
+I would like for it to be non-cheaty, but right now, the calculations seem to give back a tiny bit more soil than it uses to flatten. I will be working on it in the future though, and hopefully I'll be able to make it match vanilla soil calculations at some point  
+
+Please contact me in case of any bugs, or if you have suggestions to improve this mod
+
+## How to use this mod
+* First install the mod, and reboot the game.
+* Hold Ctrl when placing foundations to uncover the foundation. Make sure to use the "no decoration" foundation if you want to be able to see the ocean since the foundation colors are applied irrespective of the terrain height.
+
+## Contact / Feedback / Bug Reports
+You can either find me on the DSP Discord's #modding channel  
+Or you can create an issue on [GitHub](https://github.com/Velociraptor115/DSPMods)  
+\- Raptor#4825
---
+++ UncoverFoundationedOceans/UncoverFoundationedOceans.cs
@@ -0,0 +1,275 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection.Emit;
+
+namespace DysonSphereProgram.Modding.UncoverFoundationedOceans
+{
+  [BepInPlugin(GUID, NAME, VERSION)]
+  [BepInProcess("DSPGAME.exe")]
+  public class Plugin : BaseUnityPlugin
+  {
+    public const string GUID = "dev.raptor.dsp.UncoverFoundationedOceans";
+    public const string NAME = "UncoverFoundationedOceans";
+    public const string VERSION = "0.0.1";
+
+    private Harmony _harmony;
+    internal static ManualLogSource Log;
+
+    private void Awake()
+    {
+      Plugin.Log = Logger;
+      _harmony = new Harmony(GUID);
+      _harmony.PatchAll(typeof(ExposeOceansPatch));
+      Plugin.Log.LogInfo("UncoverFoundationedOceans Awake() called");
+    }
+
+    private void OnDestroy()
+    {
+      Plugin.Log.LogInfo("UncoverFoundationedOceans OnDestroy() called");
+      _harmony?.UnpatchSelf();
+      Plugin.Log = null;
+    }
+  }
+
+
+  [HarmonyPatch]
+  class ExposeOceansPatch
+  {
+    static Dictionary<int, int> previousModLevel = new Dictionary<int, int>();
+    static Dictionary<int, int> newModLevel = new Dictionary<int, int>();
+    static bool toExpose;
+
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(PlanetFactory), nameof(PlanetFactory.FlattenTerrainReform))]
+    static void Patch(ref PlanetFactory __instance)
+    {
+      var indicesToReform = newModLevel.Keys;
+      var data = __instance.planet.data;
+
+      foreach (var index in indicesToReform)
+      {
+        var newLevel = newModLevel[index];
+        var modDiff = newLevel - previousModLevel[index];
+
+        if (!toExpose)
+        {
+          if (modDiff >= 0)
+          { 
+            data.SetModLevel(index, newLevel);
+            SetDirtyFlags(__instance.planet, index, newLevel);
+          }
+        }
+        else
+        {
+          if (modDiff <= 0)
+          {
+            data.SetModLevel(index, newLevel);
+            SetDirtyFlags(__instance.planet, index, newLevel);
+          }
+        }
+      }
+    }
+
+    static void AddModLevelForIndex(int index, int prevMod, int modChange)
+    {
+      previousModLevel[index] = prevMod;
+      newModLevel[index] = toExpose ? (3 - modChange) : modChange;
+    }
+
+    [HarmonyTranspiler]
+    [HarmonyPatch(typeof(PlanetFactory), nameof(PlanetFactory.ComputeFlattenTerrainReform))]
+    static IEnumerable<CodeInstruction> InterceptIndices(IEnumerable<CodeInstruction> code, ILGenerator generator)
+    {
+      var matcher = new CodeMatcher(code, generator);
+
+      const string indexLocMatch = nameof(indexLocMatch);
+      const string modChangeLocMatch = nameof(modChangeLocMatch);
+      const string previousModLevelLocMatch = nameof(previousModLevelLocMatch);
+      const string modDiffLocMatch = nameof(modDiffLocMatch);
+
+      matcher.MatchForward(
+        true
+        , new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(PlanetFactory), nameof(PlanetFactory.tmp_levelChanges)))
+        , new CodeMatch(OpCodes.Ldloc_S, name: indexLocMatch)
+        , new CodeMatch(OpCodes.Ldloc_S, name: modChangeLocMatch)
+        , new CodeMatch(OpCodes.Callvirt, AccessTools.PropertySetter(typeof(Dictionary<int, int>), "Item"))
+      );
+
+      var indexLocal = matcher.NamedMatch(indexLocMatch).operand;
+      var modChangeLocal = matcher.NamedMatch(modChangeLocMatch).operand;
+
+      Plugin.Log.LogDebug(matcher.Pos);
+
+      matcher.MatchBack(
+        true
+        , new CodeMatch(OpCodes.Ldloc_S, modChangeLocal)
+        , new CodeMatch(OpCodes.Ldloc_S, name: previousModLevelLocMatch)
+        , new CodeMatch(OpCodes.Blt)
+      );
+
+      var previousModLevelLocal = matcher.NamedMatch(previousModLevelLocMatch).operand;
+
+      Plugin.Log.LogDebug(matcher.Pos);
+
+      matcher.MatchBack(
+        true
+        , new CodeMatch(OpCodes.Ldloc_S, modChangeLocal)
+        , new CodeMatch(OpCodes.Ldloc_S, previousModLevelLocal)
+        , new CodeMatch(OpCodes.Sub)
+        , new CodeMatch(OpCodes.Stloc_S, name: modDiffLocMatch)
+      );
+
+      var modDiffLocal = matcher.NamedMatch(modDiffLocMatch).operand;
+
+      Plugin.Log.LogDebug(matcher.Pos);
+
+      matcher.Advance(1);
+      
+      matcher.Insert(
+          new CodeInstruction(OpCodes.Ldloc_S, indexLocal)
+        , new CodeInstruction(OpCodes.Ldloc_S, previousModLevelLocal)
+        , new CodeInstruction(OpCodes.Ldloc_S, modChangeLocal)
+        , new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(ExposeOceansPatch), nameof(ExposeOceansPatch.AddModLevelForIndex)))
+      );
+
+      return matcher.InstructionEnumeration();
+    }
+
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(PlanetFactory), nameof(PlanetFactory.ComputeFlattenTerrainReform))]
+    static void PreCompute()
+    {
+      toExpose = (CombineKey.currModifier & CombineKey.CTRL_COMB) == CombineKey.CTRL_COMB;
+      previousModLevel.Clear();
+      newModLevel.Clear();
+    }
+
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(PlanetFactory), nameof(PlanetFactory.ComputeFlattenTerrainReform))]
+    static void PostCompute(ref PlanetFactory __instance, ref int __result)
+    {
+      __instance.tmp_levelChanges.Clear();
+
+      var indicesToReform = newModLevel.Keys;
+      var heightData = __instance.planet.data.heightData;
+      var realRadius = __instance.planet.realRadius;
+      int soilRequired = 0;
+
+      var totalCount = indicesToReform.Count;
+      var effectiveCount = 0;
+
+      foreach (var index in indicesToReform)
+      {
+        var modDiff = newModLevel[index] - previousModLevel[index];
+
+        if (!toExpose)
+        {
+          if (modDiff > 0)
+          {
+            var heightDiff = realRadius + 0.2f - (heightData[index] * 0.01f);
+            //if (heightDiff < 0f)
+            //  heightDiff *= 2f;
+            soilRequired += UnityEngine.Mathf.FloorToInt(100f * modDiff * heightDiff * 0.3333333f);
+            effectiveCount++;
+          }
+        }
+        else
+        {
+          if (modDiff < 0)
+          {
+            var heightDiff = realRadius + 0.2f - (heightData[index] * 0.01f);
+            //if (heightDiff < 0f)
+            //  heightDiff *= 2f;
+            soilRequired += UnityEngine.Mathf.FloorToInt(100f * modDiff * heightDiff * 0.3333333f);
+            effectiveCount++;
+          }
+        }
+      }
+
+      //Plugin.Log.LogDebug($"Total: {totalCount}; Effective: {effectiveCount}");
+
+      __result = soilRequired;
+    }
+
+    [HarmonyTranspiler]
+    [HarmonyPatch(typeof(PlanetGrid), nameof(PlanetGrid.ReformSnapTo))]
+    static IEnumerable<CodeInstruction> PatchToSelectAll(IEnumerable<CodeInstruction> code, ILGenerator generator)
+    {
+      var matcher = new CodeMatcher(code, generator);
+
+      const string ifConditionEntryLabelMatch = nameof(ifConditionEntryLabelMatch);
+
+      matcher.MatchForward(
+        false
+        , new CodeMatch(OpCodes.Ldarg_S)
+        , new CodeMatch(OpCodes.Ldloc_S)
+        , new CodeMatch(OpCodes.Callvirt, AccessTools.Method(typeof(PlatformSystem), nameof(PlatformSystem.IsTerrainReformed)))
+        , new CodeMatch(OpCodes.Brtrue)
+        , new CodeMatch(OpCodes.Ldloc_S)
+        , new CodeMatch(OpCodes.Ldarg_3)
+        , new CodeMatch(OpCodes.Bne_Un, name: ifConditionEntryLabelMatch)
+      );
+      
+      Plugin.Log.LogDebug(matcher.Pos);
+
+      var ifConditionEntryLabel = matcher.NamedMatch(ifConditionEntryLabelMatch).operand;
+
+      matcher.Insert(
+        new CodeInstruction(OpCodes.Br, ifConditionEntryLabel)
+      );
+
+      return matcher.InstructionEnumeration();
+    }
+
+    [HarmonyReversePatch]
+    [HarmonyPatch(typeof(PlanetData), nameof(PlanetData.AddHeightMapModLevel))]
+    static void SetDirtyFlags(PlanetData instance, int index, int level)
+    {
+      IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> code, ILGenerator generator)
+      {
+        var extractedCode = PatchHelpers.ExtractWithinIfBlock(code, generator
+          , new CodeMatch(OpCodes.Callvirt, AccessTools.Method(typeof(PlanetRawData), nameof(PlanetRawData.AddModLevel)))
+          , new CodeMatch(OpCodes.Brfalse));
+        return extractedCode;
+      }
+
+      _ = Transpiler(null, null);
+      throw new NotImplementedException("Stub");
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(PlanetRawData), nameof(PlanetRawData.AddModLevel))]
+    static void AlwaysReturnTrue(ref bool __result)
+    {
+      __result = true;
+    }
+  }
+
+  public static class PatchHelpers
+  {
+    public static IEnumerable<CodeInstruction> ExtractWithinIfBlock(IEnumerable<CodeInstruction> code, ILGenerator generator, params CodeMatch[] condition)
+    {
+      var matcher = new CodeMatcher(code, generator);
+      matcher.MatchForward(true, condition);
+
+      var ifBlockExitLabel = (Label)matcher.Operand;
+
+      matcher.Advance(1);
+      var startPos = matcher.Pos;
+
+      while (!matcher.Labels.Contains(ifBlockExitLabel))
+        matcher.Advance(1);
+      var endPos = matcher.Pos;
+
+      matcher.Set(OpCodes.Ret, null);
+      return matcher.InstructionsInRange(startPos, endPos);
+    }
+  }
+}
