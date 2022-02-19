ae1f032861312d085724699cb021dcb31a62a479
Velociraptor115 <Velociraptor115@users.noreply.github.com>
2022-02-19 11:40:48 +0530

    Revamp InfiniteInventory to give a choice between all or only unlocked items

--- ExposeCreativeMode/InfiniteInventory.cs
+++ ExposeCreativeMode/InfiniteInventory.cs
@@ -1,43 +1,51 @@
 using System;
-using System.IO;
 using System.Collections.Generic;
-using System.Reflection.Emit;
-using BepInEx;
-using BepInEx.Logging;
+using System.Linq;
 using HarmonyLib;
 using UnityEngine;
-using CommonAPI;
-using CommonAPI.Systems;
 
 namespace DysonSphereProgram.Modding.ExposeCreativeMode
 {
   public class InfiniteInventory
   {
     private readonly Player player;
+    private readonly StorageComponent infiniteInventory;
+    
     private int? sandRestore;
     StorageComponent infiniteInventoryRestore;
-    StorageComponent infiniteInventory;
+
+    private int prevInventorySize;
+    private IList<int> cachedItemIds;
+    private float cachedTime;
 
     public bool IsEnabled;
+    public bool IncludeLocked = true;
     public StorageComponent storage => infiniteInventory;
 
     public InfiniteInventory(Player player)
     {
       this.player = player;
+      infiniteInventory = new StorageComponent(LDB.items.Length)
+      {
+        type = EStorageType.Filtered
+      };
     }
 
     public void Enable()
     {
       sandRestore = player.sandCount;
       infiniteInventoryRestore = player.package;
-      UIRoot.instance?.uiGame.TogglePlayerInventory();
-      // Force the UI to recalculate stuff
+      
       // Because we change the storage component itself, the UI will not know the
       // underlying object has changed entirely and will continue to display
-      // the old storage component till we close and reopen it. Hence the TogglePlayerInventory()
-      infiniteInventory = Create(LDB.items.dataArray);
-      player.package = infiniteInventory;
-      UIRoot.instance?.uiGame.TogglePlayerInventory();
+      // the old storage component till we close and reopen it.
+      using (InventoryWindowDeactivatedScope)
+      {
+        RefreshCachedItemIds();
+        infiniteInventory.SetSize(GetRequiredSize(cachedItemIds.Count));
+        player.package = infiniteInventory;
+        prevInventorySize = infiniteInventory.size;
+      }
       UpdateUI();
 
       IsEnabled = true;
@@ -48,12 +56,10 @@ namespace DysonSphereProgram.Modding.ExposeCreativeMode
     {
       if (infiniteInventoryRestore != null)
       {
-        UIRoot.instance?.uiGame.TogglePlayerInventory();
-        player.package = infiniteInventoryRestore;
+        using (InventoryWindowDeactivatedScope)
+          player.package = infiniteInventoryRestore;
         infiniteInventoryRestore = null;
-        UIRoot.instance?.uiGame.TogglePlayerInventory();
       }
-      infiniteInventory = null;
       if (sandRestore.HasValue)
         player.sandCount = sandRestore.Value;
       sandRestore = null;
@@ -91,58 +97,81 @@ namespace DysonSphereProgram.Modding.ExposeCreativeMode
       if (!IsEnabled)
         return;
 
-      var items = LDB.items.dataArray;
-      var requiredSize = GetRequiredSize(items);
-      if (this.player.package.size != requiredSize)
+      if (Time.time - cachedTime > 1f)
+        RefreshCachedItemIds();
+      var requiredSize = GetRequiredSize(cachedItemIds.Count);
+      if (player.package.size != prevInventorySize)
       {
-        infiniteInventoryRestore.SetSize(this.player.package.size);
-        this.player.package.SetSize(requiredSize);
+        // Probably an inventory size upgrade, so we need to forward it to the actual inventory
+        infiniteInventoryRestore.SetSize(player.package.size);
       }
-      var inventory = this.player.package.grids;
-      for (int i = 0; i < items.Length; ++i)
-      {
-        var item = items[i];
-        inventory[i].itemId = item.ID;
-        inventory[i].filter = item.ID;
-        inventory[i].stackSize = 30000;
-        inventory[i].count = 9999;
-      }
-
+      if (player.package.size != requiredSize)
+        player.package.SetSize(requiredSize);
+      prevInventorySize = player.package.size;
+      RestockInfiniteInventory(cachedItemIds);
       player.sandCount = 999999;
     }
 
-    private static int GetRequiredSize(ICollection<ItemProto> items)
+    private void RefreshCachedItemIds()
     {
-      var itemCount = items.Count;
+      cachedItemIds = GetItemIdsFiltered();
+      cachedTime = Time.time;
+    }
+
+    private void RestockInfiniteInventory(IList<int> itemIds)
+    {
+      var grids = infiniteInventory.grids;
+      for (int i = 0; i < itemIds.Count; ++i)
+      {
+        grids[i].itemId = itemIds[i];
+        grids[i].filter = itemIds[i];
+        grids[i].stackSize = 30000;
+        grids[i].count = 9999;
+      }
+      for (int i = itemIds.Count; i < infiniteInventory.size; i++)
+      {
+        grids[i].itemId = 0;
+        // We need to do this, because filter <= 0 is considered fair game by the UI
+        grids[i].filter = int.MaxValue;
+        grids[i].stackSize = 0;
+        grids[i].count = 0;
+      }
+      infiniteInventory.NotifyStorageChange();
+    }
+
+    private IList<int> GetItemIdsFiltered() => LDB.items.dataArray.Where(itemFilter).Select(x => x.ID).ToList();
+    private Func<ItemProto, bool> itemFilter => IncludeLocked ? AllItemsPredicate : OnlyUnlockedItemsPredicate; 
+    private static bool OnlyUnlockedItemsPredicate(ItemProto item) => GameMain.history.ItemUnlocked(item.ID);
+    private static bool AllItemsPredicate(ItemProto item) => true;
+
+    private static int GetRequiredSize(int count)
+    {
       var colCount = UIRoot.instance.uiGame.inventory.colCount;
       // We need to set extra size, otherwise the UI bugs out when dropping items on the extra space
       // by throwing an IndexOutOfRange exception
-      return itemCount + (itemCount % colCount > 0 ? colCount - (itemCount % colCount) : 0);
+      return count + (count % colCount > 0 ? colCount - (count % colCount) : 0);
     }
 
-    private static StorageComponent Create(IList<ItemProto> items)
+    private static InventoryWindowDeactivationContext InventoryWindowDeactivatedScope =>
+      new InventoryWindowDeactivationContext();
+
+    private readonly ref struct InventoryWindowDeactivationContext
     {
-      var size = GetRequiredSize(items);
-      var storage = new StorageComponent(size);
-      storage.type = EStorageType.Filtered;
-      for (int i = 0; i < items.Count; ++i)
+      private readonly bool wasActive;
+      private readonly UIGame uiGame;
+      public InventoryWindowDeactivationContext()
       {
-        var item = items[i];
-        storage.grids[i].itemId = item.ID;
-        storage.grids[i].filter = item.ID;
-        storage.grids[i].stackSize = 30000;
-        storage.grids[i].count = 9999;
-      }
-      for (int i = items.Count; i < size; i++)
-      {
-        storage.grids[i].itemId = 0;
-        // We need to do this, because filter <= 0 is considered fair game by the UI
-        storage.grids[i].filter = int.MaxValue;
-        storage.grids[i].stackSize = 0;
-        storage.grids[i].count = 0;
+        uiGame = UIRoot.instance && UIRoot.instance.uiGame ? UIRoot.instance.uiGame : null;
+        wasActive = uiGame && uiGame.inventory && uiGame.inventory.active;
+        if (wasActive)
+          uiGame.ShutPlayerInventory();
       }
 
-      return storage;
+      public void Dispose()
+      {
+        if (wasActive)
+          uiGame.OpenPlayerInventory();
+      }
     }
   }
 
