7159ee5f397c65e76970ee6fc38e22e873c11ef8
Velociraptor115 <Velociraptor115@users.noreply.github.com>
2022-02-25 11:49:52 +0530

    [UIEnhancements] Modify slider logic for station storage max

--- UIEnhancements/EditableStationStorageMax.cs
+++ UIEnhancements/EditableStationStorageMax.cs
@@ -17,6 +17,9 @@ public class EditableStationStorageMax: EnhancementBase
   public static List<Text> patchedTexts = new List<Text>();
   public static List<InputField> patchedInputFields = new List<InputField>();
 
+  public static ConfigEntry<int> sliderStep;
+  public static ConfigEntry<int> sliderStepPercent;
+
   [HarmonyPostfix]
   [HarmonyPatch(typeof(UIStationWindow), nameof(UIStationWindow._OnCreate))]
   public static void PatchStationWindow(ref UIStationWindow __instance)
@@ -74,25 +77,10 @@ public class EditableStationStorageMax: EnhancementBase
     return inputFieldCtx.inputField;
   }
   
-  [HarmonyPrefix]
-  [HarmonyPatch(typeof(UIStationStorage), nameof(UIStationStorage.RefreshValues))]
-  public static void InterceptStationStorageValues(ref UIStationStorage __instance, ref int? __state)
-  {
-    __state = null;
-    if (__instance.station == null)
-      return;
-    if (__instance.index >= __instance.station.storage.Length)
-      return;
-
-    __state = __instance.station.storage[__instance.index].max;
-  }
-  
   [HarmonyPostfix]
   [HarmonyPatch(typeof(UIStationStorage), nameof(UIStationStorage.RefreshValues))]
-  public static void PatchStationStorageRefreshValues(ref UIStationStorage __instance, ref int? __state)
+  public static void PatchStationStorageRefreshValues(ref UIStationStorage __instance)
   {
-    if (!__state.HasValue)
-      return;
     if (__instance.station == null)
       return;
     if (__instance.index >= __instance.station.storage.Length)
@@ -100,7 +88,60 @@ public class EditableStationStorageMax: EnhancementBase
 
     var proto = LDB.items.Select(__instance.stationWindow.factory.entityPool[__instance.station.entityId].protoId);
     __instance.maxSlider.maxValue = proto.prefabDesc.stationMaxItemCount + __instance.GetAdditionStorage();
-    __instance.maxSlider.value = __state.Value;
+    __instance.maxSlider.value = __instance.station.storage[__instance.index].max;
+  }
+  
+  [HarmonyPrefix]
+  [HarmonyPatch(typeof(UIStationStorage), nameof(UIStationStorage.OnMaxSliderValueChange))]
+  public static void InterceptSliderValueChange(ref UIStationStorage __instance, ref float val)
+  {
+    if (__instance.station == null)
+      return;
+    if (__instance.index >= __instance.station.storage.Length)
+      return;
+
+    if ((int)val == __instance.station.storage[__instance.index].max) // This is being set from the input field 
+      return;
+    
+    if ((int)val == (int)__instance.maxSlider.maxValue)
+      return;
+
+    var step = sliderStep.Value;
+    if (sliderStepPercent.Value != 0)
+      step = (int)__instance.maxSlider.maxValue * sliderStepPercent.Value / 100;
+
+    val -= (int)val % step;
+  }
+  
+  [HarmonyTranspiler]
+  [HarmonyPatch(typeof(UIStationStorage), nameof(UIStationStorage.RefreshValues))]
+  public static IEnumerable<CodeInstruction> PatchRefreshValues(IEnumerable<CodeInstruction> code, ILGenerator generator)
+  {
+    var matcher = new CodeMatcher(code, generator);
+
+    matcher.SearchForward(ci => ci.Calls(AccessTools.PropertySetter(typeof(Slider), nameof(Slider.value))));
+    var valueSetEndPos = matcher.Pos;
+    matcher.MatchBack(false,
+      new CodeMatch(ci => ci.IsLdarg(0)),
+      new CodeMatch(ci => ci.LoadsField(AccessTools.Field(typeof(UIStationStorage), nameof(UIStationStorage.maxSlider))))
+    );
+    var valueSetStartPos = matcher.Pos;
+
+    matcher.RemoveInstructionsInRange(valueSetStartPos, valueSetEndPos);
+
+    matcher.Start();
+    matcher.SearchForward(ci => ci.Calls(AccessTools.PropertySetter(typeof(Slider), nameof(Slider.maxValue))));
+    var maxValueSetEndPos = matcher.Pos;
+    matcher.MatchBack(false,
+      new CodeMatch(ci => ci.IsLdarg(0)),
+      new CodeMatch(ci => ci.LoadsField(AccessTools.Field(typeof(UIStationStorage), nameof(UIStationStorage.maxSlider))))
+    );
+    var maxValueSetStartPos = matcher.Pos;
+    
+    matcher.RemoveInstructionsInRange(maxValueSetStartPos, maxValueSetEndPos);
+    
+
+    return matcher.InstructionEnumeration();
   }
   
   [HarmonyTranspiler]
@@ -142,7 +183,12 @@ public class EditableStationStorageMax: EnhancementBase
   }
   protected override void UseConfig(ConfigFile configFile)
   {
-    
+    sliderStep = configFile.Bind(ConfigSection, "Slider Step", 100);
+    sliderStepPercent =
+      configFile.Bind(ConfigSection, "Slider Step %", 0, new ConfigDescription(
+        "If this is non-zero, the slider step will be a percentage of the maximum"
+        , new AcceptableValueRange<int>(0, 100)
+      ));
   }
   protected override void Patch(Harmony _harmony)
   {
